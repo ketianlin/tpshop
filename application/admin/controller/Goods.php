510b73b4392b9c4e5d638271945c1c65debb3fe4
a-ke <642028114@qq.com>
2020-12-22 17:32:23 +0800

    上传图片、分页、缩略图，路由，表单令牌等等操作

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -3,16 +3,24 @@
 namespace app\admin\controller;
 
 use think\Config;
-use think\Controller;
+use think\Collection;
 use app\admin\model\Goods as GoodsModel;
+use think\Image;
 use think\Request;
 
 class Goods extends Base
 {
     public function index(){
-        $data = GoodsModel::select();
-        $list = (new Collection($data))->toArray();
-        return view(null, ['list'=>$list]);
+        // 接收keyword参数
+        $keyword = input('keyword');
+        $condition = [];
+        if ( ! empty($keyword)){
+            $condition['goods_name'] = ['like', "%$keyword%"];
+        }
+        $data = GoodsModel::where($condition)->order('id desc')->paginate(5, false, [
+            'query'=>['keyword'=>$keyword]
+        ]);
+        return view(null, ['list'=>$data]);
     }
     public function test(){
 //        $goods = new GoodsModel();
@@ -32,7 +40,7 @@ class Goods extends Base
     }
 
     private $rule = [
-        'goods_name|商品名称'=>'require|max:55',
+        'goods_name|商品名称'=>'require|token|max:55',
         'goods_price|商品价格'=>'require|float|gt:0',
         'goods_number|商品数量'=>'require|integer|gt:0'
     ];
@@ -53,10 +61,25 @@ class Goods extends Base
         if ($res !== true){
             return $this->error($res);
         }
+        //商品logo图片的修改
+        $file = request()->file('logo');
+
+        if (empty($file)){
+            unset($params['goods_logo']);
+        }else{
+            $params['goods_logo'] = $this->upload_logo();
+            $goods_logo = GoodsModel::where('id',$id)->value('goods_logo');
+        }
         //处理数据（修改数据到数据表）
         GoodsModel::update($params, ['id'=>$id], true);
+        //删除原图片
+        if (isset($goods_logo) && $goods_logo){
+            if (file_exists('.'.$goods_logo)){
+                unlink('.'.$goods_logo);
+            }
+        }
         //返回（跳转页面）
-        $this->success('操作成功', 'index');
+        $this->success('操作成功', 'admin/goods/index');
     }
 
     public function edit(){
@@ -77,8 +100,11 @@ class Goods extends Base
         if ($result !== true){
             return $this->error($result);
         }
+        //文件上传
+        $data['goods_logo'] = $this->upload_logo();
+        //添加数据到数据表  第二个参数true表示过滤非数据表字段
         GoodsModel::create($data, true);
-        return $this->success('添加成功', 'index');
+        return $this->success('添加成功', 'admin/goods/index');
     }
 
     public function delete(){
@@ -103,5 +129,33 @@ class Goods extends Base
             return $this->error(Config::get('msg.sys_err_msg'));
         }
         return view(null, ['goods'=>$goods]);
+    }
+
+    private function upload_logo($name = 'logo')
+    {
+        // 获取表单上传文件 例如上传了001.jpg
+        $file = request()->file($name);
+
+        //判断 是否上传了文件
+        if (empty($file)){
+            return $this->error('没有上传文件');
+        }
+        // 移动到框架应用根目录/public/uploads/ 目录下
+        $fileValidate = ['size' => 100*1024*1024, 'ext' => 'jpg,png,gif,jpeg'];
+        $info = $file->validate($fileValidate)->move(ROOT_PATH . 'public' . DS . 'uploads');
+
+        if($info){
+            //上传成功  拼接图片的访问路径  /uploads/20190709/fssdsahfdskasa.jpg
+            $goods_logo = DS.'uploads'.DS.$info->getSaveName();
+            //生成缩略图  \think\Image类  保存
+            //打开图片
+            $image = Image::open('.'.$goods_logo);
+            // 生成缩略图  保存图片
+            $image->thumb(300, 250)->save('.'.$goods_logo);
+            return $goods_logo;
+        }
+        // 上传失败获取错误信息
+        $error_msg = $file->getError();
+        return $this->error($error_msg);
     }
 }
--- application/route.php
+++ application/route.php
@@ -8,14 +8,41 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
+use think\Route;
+////定义路由
+////后台登录页  login
+//Route::any('login', 'admin/login/login', ['ext' => 'html']);
+////后台商品列表
+//Route::get('goods', 'admin/goods/index', ['ext' => 'html']);
+////后台商品新增
+//Route::get('goods_create', 'admin/goods/add', ['ext'=>'html']);
+////后台商品新增-保存
+//Route::post('goods_save', 'admin/goods/save', ['ext'=>'html']);
+////后台商品修改
+//Route::get('goods_edit/:id', 'admin/goods/edit', ['ext'=>'html'], ['id'=>'\d+']);
+////后台商品修改-保存
+//Route::post('goods_update/:id', 'admin/goods/update', ['ext'=>'html'], ['id'=>'\d+']);
+//Route::rule('goods_update/:id', 'admin/goods/update', 'POST', ['ext' => 'html'], ['id' => '\d+']);
+//后台商品详情
+//Route::get('goods_read/:id', 'admin/goods/read', ['ext' => 'html'], ['id' => '\d+']);
+//Route::rule('goods_read/:id', 'admin/goods/read', 'GET' , ['ext' => 'html'], ['id' => '\d+']);
+//后台商品删除
+//Route::get('goods_delete/:id', 'admin/goods/delete', ['ext' => 'html'], ['id' => '\d+']);*/
 
-return [
-    '__pattern__' => [
-        'name' => '\w+',
-    ],
-    '[hello]'     => [
-        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
-        ':name' => ['index/hello', ['method' => 'post']],
-    ],
+//路由分组
+/*Route::group('admin', function(){
+    //后台商品详情  goods_read/33.html   变成  goods_read_33.html
+//    Route::get('goods_read/:id', 'admin/goods/read', ['ext' => 'html'], ['id' => '\d+']);
+    Route::get('goods_read_<id>', 'admin/goods/read', ['ext' => 'html'], ['id' => '\d+']);
+    //后台商品删除
+    Route::get('goods_delete/:id', 'admin/goods/delete', ['ext' => 'html'], ['id' => '\d+']);
+});
 
-];
+//路由分组
+Route::group('home', function(){
+    //前台登录   www.tpshop.com/home/login.html
+    Route::get('login', 'home/login/login', ['ext' => 'html']);
+});*/
+
+//参数  子域名 ， 对应的模块
+Route::domain('admin', 'admin');
